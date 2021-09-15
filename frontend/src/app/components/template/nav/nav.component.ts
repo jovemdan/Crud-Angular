13dcdd6874ade497b4ab06c542a0ff59bb5a580c
jovemdan <twiixplays@gmail.com>
2021-09-15 15:01:55 -0300

    Instalação do Bulma css e modificação no layout

--- frontend/src/app/components/template/nav/nav.component.ts
+++ frontend/src/app/components/template/nav/nav.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  @ViewChild('navBurger') navBurger: ElementRef
+  @ViewChild('navMenu') navMenu: ElementRef
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active')
+    this.navMenu.nativeElement.classList.toggle('is-active')
   }
-
 }
