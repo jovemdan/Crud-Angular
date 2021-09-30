a7e9c42b82f3349513ff6ddeeca8711d1ff24e5c
Danilo Menezes <danilo.menezes@guide.com.br>
2021-09-29 23:25:17 -0300

    Ajustes na home

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core'
 import { Product } from 'src/app/components/product/product.model'
-import { HeaderService } from 'src/app/components/template/header/header.service'
 import { ProductService } from 'src/app/components/product/product.service'
 
 @Component({
@@ -9,22 +8,17 @@ import { ProductService } from 'src/app/components/product/product.service'
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  products: Product[]
+  products: Product[] = []
+
   constructor(
-    private headerService: HeaderService,
     private productService: ProductService
-  ) {
-    headerService.headerData = {
-      title: 'Início',
-      icon: 'home',
-      routeUrl: '',
-    }
-  }
+  ) {  }
+
+  
 
   ngOnInit(): void {
     this.productService.read().subscribe((products) => {
-      this.products = products
-      console.log(products)
+      this.products = products.slice(2)
     })
   }
 }
